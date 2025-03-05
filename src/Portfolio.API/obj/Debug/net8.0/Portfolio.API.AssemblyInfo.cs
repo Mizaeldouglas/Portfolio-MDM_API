cff87c8f0a40f7d70747f136063cd061b3d673af
MizaelDouglasDeMello <mizaeldouglas4@gmail.com>
2025-03-05 10:49:55 -0300

    add packages Application layers: MediatR, FluentValidation,AutoMapper and DependencyInjection.Abstractions.

--- src/Portfolio.API/obj/Debug/net8.0/Portfolio.API.AssemblyInfo.cs
+++ src/Portfolio.API/obj/Debug/net8.0/Portfolio.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53ee01552327ac158519652bca0e4762d7bc1eee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
